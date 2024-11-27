715aebfad6bc079c06866172a08ff511897585ac
hyo <vlsjt3@naver.com>
2024-11-27 21:41:28 +0900

    User Decorator 추가

--- src/res/user/user.controller.ts
+++ src/res/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, Post, Req, UnauthorizedException, UseGuards } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { User } from "src/decorators/user.decorator";
 
 @Controller('/user')
 export class UserController {
@@ -21,9 +22,9 @@ export class UserController {
 
     @UseGuards(JwtAuthGuard)
     @Get('user-info/:id')
-    async getUserInfo(@Req() req, @Param('id') id: string) {
-        // JwtAuthGuard를 거치면서 로그인 여부 확인
-        const user = req?.user;
+    async getUserInfo(@User() user, @Param('id') id: string) {
+        // JwtAuthGuard를 거치면서 로그인 여부 확인 후, User 데코레이터를 사용해 회원 정보 받아옴
+        console.log('id : ', user.id);
 
         if (user.id !== id) {
             throw new UnauthorizedException();
